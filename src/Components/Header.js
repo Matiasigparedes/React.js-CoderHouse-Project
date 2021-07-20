e7a12104aa5975bc0ab53de07633c89eb386c84a
Matías Ignacio Paredes Marquéz <matias.paredesm95@outlook.com>
2021-07-20 16:33:50 -0400

    React version 0.2

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <header>
+            <h1>Titulo de mi react app</h1>
+            <h4>Descripcion general de mi react app...</h4>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/Main/App.js
@@ -0,0 +1,13 @@
+import Header from "../Components/Header";
+import NavBar from "../Components/NavBar"
+
+function App(){
+    return(
+        <>
+            <Header/>,
+            <NavBar/>
+        </>
+    );
+}
+
+export default App;
