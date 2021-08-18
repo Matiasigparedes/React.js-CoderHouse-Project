d2441fe16a8a77b7dac52b5d30e0b1788fc9416d
Matías Ignacio Paredes Marquéz <matias.paredesm95@outlook.com>
2021-08-18 15:02:24 -0400

    React version 0.7

--- src/Components/ItemCounter/ItemCounter.js
+++ src/Components/ItemCounter/ItemCounter.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import ItemDetail from '../ItemDetail/ItemDetail';
 
 function ItemCount({stock, initial, onAdd}){
 
--- src/Components/ItemDetail/ItemDetail.js
+++ src/Components/ItemDetail/ItemDetail.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import ItemCounter from '../ItemCounter/ItemCounter';
 
+//rcf
 const ItemDetail = ({ producto }) => {
+
+  const [cantidadDefinida, setCantidadDefinida] = useState(0);
+
+  const onAdd = (cantidad) => {
+    setCantidadDefinida(cantidad);
+  };
+
   return (
     <div className="card" style={{ width: "15rem", margin: "10px" }}>
       <img
@@ -12,9 +23,30 @@ const ItemDetail = ({ producto }) => {
       <div className="card-body">
         <h5 className="card-title"> {producto.nombre} </h5>
         <p className="card-text"> {producto.precio} </p>
+
+        {
+          cantidadDefinida == 0 ?
+          (
+            <ItemCounter initial={1} stock = {10} onAdd={onAdd}/>
+          ) : (
+            <>
+            <Link to="/cart">
+              <button>Terminar compra</button>
+            </Link>
+
+            <button onClick={() => setCantidadDefinida(0)}>
+              Editar compra
+            </button>
+            </>
+          )
+        }
+        <Link to="/">
+          Seguir comprando...
+        </Link>
       </div>
     </div>
   );
 };
 
 export default ItemDetail;
+
